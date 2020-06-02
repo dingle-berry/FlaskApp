41f90cf8bcdddf5eebce725ca64ec478b25fdf24
Chance Boyd <cboyd@doubleds.local>
2020-06-01 22:49:50 -0400

    Video 10 of the tut done, lots of stuff added.

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5e7767fa8e90916f723b0f57a249ac90b93ff412dbf19fd76e2c6b0265763e06'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskapp import routes
--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskapp.models import User
 
 class RegistrationForm(FlaskForm):
   username = StringField('Username',
@@ -11,6 +14,16 @@ class RegistrationForm(FlaskForm):
   password_confirm = PasswordField('Password Confirm', validators=[DataRequired(), EqualTo('password')])
   submit = SubmitField('Sign Up')
 
+  def validate_username(self, username):
+      user = User.query.filter_by(username=username.data).first()
+      if user:
+          raise ValidationError('Username already in use.')
+
+  def validate_email(self, email):
+      check_email = User.query.filter_by(email=email.data).first()
+      if check_email:
+          raise ValidationError('An account with this email already exists. Try logging in?')
+
 class LoginForm(FlaskForm):
   username = StringField('Username',
                          validators=[DataRequired(), Length(min=2, max=20)])
@@ -18,3 +31,27 @@ class LoginForm(FlaskForm):
   submit = SubmitField('Sign in')
   remember = BooleanField('Remember Me')
 
+class UpdateAccountForm(FlaskForm):
+  username = StringField('Username',
+                         validators=[DataRequired(), Length(min=2, max=20)])
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+  submit = SubmitField('Update')
+
+  def validate_username(self, username):
+      if username.data != current_user.username:
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+              raise ValidationError('Username already in use.')
+
+  def validate_email(self, email):
+      if email.data != current_user.email:
+        check_email = User.query.filter_by(email=email.data).first()
+        if check_email:
+              raise ValidationError('An account with this email already exists. Try logging in?')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
